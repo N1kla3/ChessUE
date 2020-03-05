5e98c049b0b9145f9f0982d7ac61cc14f899e0c4
Nicolas <kolya.vladimirsky@gmail.com>
2020-03-05 22:48:46 +0300

    init project

---
+++ Source/ChessUE/ChessUEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChessUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESSUE_API AChessUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
